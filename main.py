f02331820d0fb990937c58dfdb34394a1fc4ff92
deepgiri761 <deepgiri761@gmail.com>
2024-09-08 20:17:05 +0530

    Use env variables and create Procfile

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
-
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -29,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
